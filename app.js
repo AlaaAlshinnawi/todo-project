b566ed1926a07caa1778df47c564383fc74d8d29
AlaaAlshinnawi <alaa.alshennawy2021@gmail.com>
2022-01-31 10:31:29 +0200

    Updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function Creating_User() {
+function My_Function() {
   let username = prompt("Please enter your name:", "");
-  if(username === null) {
-    username="Invalid";
+  if (username === null) {
+    username = "Invalid";
   }
   let usergender = prompt("Please enter your gender:", "");
   switch (useregender) {
@@ -15,16 +15,16 @@ function Creating_User() {
       usergender = "null";
 
   }
-  if(usergender === null) {
-    usergender="Invalid";
+  if (usergender === null) {
+    usergender = "Invalid";
   }
 
   let userage = prompt("Please enter your age:", "");
   if (userage <= 0) {
     alert("Your Age Should Be More Than One And Not Equal To Zero ");
   }
-  if(userage === null) {
-    userage="Invalid";
+  if (userage === null) {
+    userage = "Invalid";
   }
   let userconfirm = confirm("do you want to skip the welcoming message?");
   if (userconfirm === false) {
@@ -40,21 +40,26 @@ function Creating_User() {
 
     }
   }
- let userq1 = prompt("Are you feeling better today?", "");
-  if(userq1 === null) {
-    userq1="Invalid";
-  }
-  let userq2 = prompt("Are you feeling better today?", "");
-  if(userq2 === null) {
-    userq2="Invalid";
-  }
-  let userq3 = prompt("Are you feeling better today?", "");
-  if(userq3 === null) {
-    userq3="Invalid";
-  }
-
 
 }
+function Creating_User() {
+  let userq1 = prompt("Are you feeling better today?", "");
+  if (userq1 === null) {
+    userq1 = "Invalid";
+  }
+  let userq2 = prompt("Are you feeling better today?", "");
+  if (userq2 === null) {
+    userq2 = "Invalid";
+  }
+  let userq3 = prompt("Are you feeling better today?", "");
+  if (userq3 === null) {
+    userq3 = "Invalid";
+  }
+
+
+
+Creating_User();
+
 var userArr = [];
 userArr.push(username);
 userArr.push(usergender);
@@ -62,7 +67,7 @@ userArr.push(userage);
 userArr.push(userq1);
 userArr.push(userq2);
 userArr.push(userq3);
-for(var i=0;i<=5 ;i++)
-{
-console.log('Element ['+ i + '}= ' + userArr[i]);
+for (var i = 0; i <= 5; i++) {
+  console.log('Element [' + i + '}= ' + userArr[i]);
+}
 }
